b0be8cbc56baa2372084c5a457d9ea5603f2ae57
Branden Palmer <brandenpalmer08@gmail.com>
2021-08-09 15:44:57 +0700

    Catch input in buildGrid method that's not an int < 101

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // build grid of divs default to 16x16
 function buildGrid(numElements = 16) {
-  // TO-DO get prompt to only accept whole int>101
-  while (numElements > 101 || isNaN(numElements))
-    numElements = prompt(
-      "Alert! You must enter a whole number no greater than 100."
-    );
-  // TO-DO get prompt to only accept whole int>101
-
   let elementWidth = 100 / numElements;
   let elementHeight = elementWidth;
   const container = document.getElementById("container");
+  // Catch input that's not an int < 101
+  while (numElements > 101 || isNotInteger(numElements))
+    numElements = prompt(
+      "Alert! You must enter a whole number no greater than 100."
+    );
   for (let i = 0; i < numElements; i++) {
     // FOR TESTING
     let testNum = 0;
@@ -26,4 +24,9 @@ function buildGrid(numElements = 16) {
     }
   }
 }
-buildGrid(2);
+
+// For use to make prompt while conditional more readable
+function isNotInteger(input) {
+  return isNaN(input) ? true : !Number.isInteger(input);
+}
+buildGrid();
