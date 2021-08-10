89267b50045afd312b7ba1a475252ae282174137
Branden Palmer <brandenpalmer@gmail.com>
2021-08-10 15:45:19 +0700

    add func to paint each element when mouse enters div

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // build grid of divs default to 16x16
 function buildGrid(numElements = 16) {
-
-    // let elementWidth = 100 / numElements;
-    // let elementHeight = elementWidth;
     const container = document.getElementById("container");
     // Catch input that's not an int < 101
     while (numElements > 101 || isNotInteger(numElements))
@@ -16,10 +13,8 @@ function buildGrid(numElements = 16) {
         for (let j = 0; j < numElements; j++) {
             const div = document.createElement("div");
             div.classList.add("grid-element");
-            //move to own func
-            // div.style.width = `${elementWidth}vw`;
-            // div.style.height = `${elementHeight}vw`;
             setElementSize(div,numElements);
+            paintElementElementOnHover(div);
             // FOR TESTING
             div.textContent = `${++testNum}`;
             // FOR TESTING
@@ -30,12 +25,16 @@ function buildGrid(numElements = 16) {
 
 // set size of div elements based on amount and view width
 function setElementSize(div, numElements){
-
     let elementWidth = 100 / numElements;
     let elementHeight = elementWidth;
     div.style.width = `${elementWidth}vw`;
     div.style.height = `${elementHeight}vw`;
     
+}
+
+// paint each element when hovered over
+function paintElementElementOnHover(div){
+    div.addEventListener('mouseenter',()=>div.style.backgroundColor = "red")
 }
 
 // For use to make prompt while conditional more readable
