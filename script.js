001cb3a8011bed0db6d1e78996db24d8d4d07316
Branden Palmer <brandenpalmer08@gmail.com>
2021-08-11 11:00:29 +0700

    rename various functions for readability reactor invalid input condition into function for readability

--- script.js
+++ script.js
@@ -2,72 +2,86 @@
 const container = document.getElementById("container");
 
 // place button before grid in its own container
-function resetButton() {
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("buttonContainer");
-    const resetButton = document.createElement("button");
-    resetButton.classList.add("resetButton");
-    buttonContainer.appendChild(resetButton);
-    resetButton.textContent = "Reset Button";
-    resetButton.addEventListener('click', resetEvent);
+function buildResetButton() {
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("buttonContainer");
+  const resetButton = document.createElement("button");
+  resetButton.classList.add("resetButton");
+  buttonContainer.appendChild(resetButton);
+  resetButton.textContent = "Reset Button";
+  resetButton.addEventListener("click", resetGrid);
 
-    // place reset button
-    const containerParent = container.parentNode;
-    containerParent.insertBefore(buttonContainer, container);
+  // place reset button
+  const containerParent = container.parentNode;
+  containerParent.insertBefore(buttonContainer, container);
 }
 
-// complete all reset actions
-function resetEvent() {
-    resetGrid();
-    buildGrid(promptForNewGrid());
-}
-
-// reset grid, used by resetButton()
+// reset the grid with a new one
 function resetGrid() {
-    let elementNodeList = container.getElementsByClassName("gridElement");
-    let elementArray = Array.from(elementNodeList);
-    for (let i = 0; i < elementArray.length; i++) {
-        container.removeChild(elementArray[i]);
-    }
+  removeGrid();
+  buildGrid(getDIMForGrid());
 }
 
-// ask user for dimensions for next grid
-function promptForNewGrid() {
-    let numElements = prompt("Enter a size for your new canvas.");
-    // Catch input that's not an int < 100
-    while (!Number.isInteger(+numElements) || numElements > 100 || isNaN(numElements))
-        numElements = prompt(
-            "Alert! You must enter a whole number no greater than 100."
-        );
-    return numElements;
+// remove all elements in current grid
+function removeGrid() {
+  let elementNodeList = container.getElementsByClassName("gridElement");
+  let elementArray = Array.from(elementNodeList);
+  for (let i = 0; i < elementArray.length; i++) {
+    container.removeChild(elementArray[i]);
+  }
+}
+
+// ask user for dimensions for new grid
+function getDIMForGrid() {
+  let numElements = prompt("Enter a size for your new canvas.");
+  // Catch input that's not an int < 100
+  while (rejectInvalidInput(numElements))
+    numElements = prompt(
+      "Alert! You must enter a whole number no greater than 100."
+    );
+  return numElements;
+}
+
+// check for proper user input
+function rejectInvalidInput(numElements) {
+  return (
+    !Number.isInteger(+numElements) ||
+    numElements > 100 ||
+    isNaN(numElements) ||
+    numElements === "" ||
+    numElements === null
+  );
 }
 
 // build grid of divs default to 16x16
 function buildGrid(numElements = 16) {
-    for (let i = 0; i < numElements; i++) {
-        for (let j = 0; j < numElements; j++) {
-            const div = document.createElement("div");
-            div.classList.add("gridElement");
-            setElementSize(div, numElements);
-            paintElementOnHover(div);
-            container.append(div);
-        }
+  for (let i = 0; i < numElements; i++) {
+    for (let j = 0; j < numElements; j++) {
+      const div = document.createElement("div");
+      div.classList.add("gridElement");
+      setElementSize(div, numElements);
+      paintElementOnHover(div);
+      container.append(div);
     }
+  }
 }
 
 // set size of div elements based on amount and view width
 function setElementSize(div, numElements) {
-    let elementWidth = 100 / numElements;
-    let elementHeight = elementWidth;
-    div.style.width = `${elementWidth}vw`;
-    div.style.height = `${elementHeight}vw`;
-
+  let elementWidth = 100 / numElements;
+  let elementHeight = elementWidth;
+  div.style.width = `${elementWidth}vw`;
+  div.style.height = `${elementHeight}vw`;
 }
 
 // paint each element when hovered over
 function paintElementOnHover(div) {
-    div.addEventListener('mouseover', () => div.style.backgroundColor = "black", { once: true })
+  div.addEventListener(
+    "mouseover",
+    () => (div.style.backgroundColor = "black"),
+    { once: true }
+  );
 }
 
-resetButton();
+buildResetButton();
 buildGrid();
